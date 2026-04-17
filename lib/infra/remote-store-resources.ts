fc12da61989b4caf1ed2a1debb852667af6d32eb
Sayali Gaikawad <gaiksaya@amazon.com>
2026-04-17 15:27:08 -0700

    Upgrade es-lint and remediate CVEs
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- lib/infra/remote-store-resources.ts
+++ lib/infra/remote-store-resources.ts
@@ -3,35 +3,35 @@ import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class RemoteStoreResources {
-    private readonly snapshotS3Bucket: Bucket
+  private readonly snapshotS3Bucket: Bucket;
 
-    private readonly bucketPolicyStatement: PolicyStatement
+  private readonly bucketPolicyStatement: PolicyStatement;
 
-    constructor(scope: Stack) {
-      this.snapshotS3Bucket = new Bucket(scope, `remote-store-${scope.stackName}`, {
-        removalPolicy: RemovalPolicy.DESTROY,
-        autoDeleteObjects: true,
-        bucketName: `${scope.stackName.toLowerCase()}`,
-      });
+  constructor(scope: Stack) {
+    this.snapshotS3Bucket = new Bucket(scope, `remote-store-${scope.stackName}`, {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      bucketName: `${scope.stackName.toLowerCase()}`,
+    });
 
-      this.bucketPolicyStatement = new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          's3:ListBucket',
-          's3:GetBucketLocation',
-          's3:ListBucketMultipartUploads',
-          's3:ListBucketVersions',
-          's3:GetObject',
-          's3:PutObject',
-          's3:DeleteObject',
-          's3:AbortMultipartUpload',
-          's3:ListMultipartUploadParts',
-        ],
-        resources: [this.snapshotS3Bucket.bucketArn, `${this.snapshotS3Bucket.bucketArn}/*`],
-      });
-    }
+    this.bucketPolicyStatement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        's3:ListBucket',
+        's3:GetBucketLocation',
+        's3:ListBucketMultipartUploads',
+        's3:ListBucketVersions',
+        's3:GetObject',
+        's3:PutObject',
+        's3:DeleteObject',
+        's3:AbortMultipartUpload',
+        's3:ListMultipartUploadParts',
+      ],
+      resources: [this.snapshotS3Bucket.bucketArn, `${this.snapshotS3Bucket.bucketArn}/*`],
+    });
+  }
 
-    public getRemoteStoreBucketPolicy() {
-      return this.bucketPolicyStatement;
-    }
+  public getRemoteStoreBucketPolicy() {
+    return this.bucketPolicyStatement;
+  }
 }
